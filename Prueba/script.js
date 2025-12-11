cb686d8a59a3d0a069c5ea0d65e52c3cd96df970
Joel120210 <l21070333@cdmadero.tecnm.mx>
2025-12-11 13:53:42 -0600

    Graaficador React

---
+++ Prueba/script.js
@@ -0,0 +1,17 @@
+// Animación de aparición al hacer scroll
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (elementTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
